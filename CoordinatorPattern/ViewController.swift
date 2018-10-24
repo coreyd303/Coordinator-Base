c7cbcbf86b528d8e796947e5b3e89f8b20e75bbd
Corey Davis <cdavis@gospotcheck.com>
2018-10-24 12:49:13 -0600

    Initial Commit

---
+++ CoordinatorPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorPattern
+//
+//  Created by Corey Davis on 10/24/18.
+//  Copyright © 2018 Corey Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
